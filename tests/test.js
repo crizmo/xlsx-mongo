1d7081ebea15e23479fcfcd7828809a1a0cd9fdc
Kurizu <83665497+crizmo@users.noreply.github.com>
2023-07-09 22:44:52 +0530

    ♻️ JS > html
    Added comments on tests

--- tests/test.js
+++ tests/test.js
@@ -1,24 +1,25 @@
 const xlsx2mongo = require('xlsx-mongo');
 const mongoose = require('mongoose');
-require('dotenv').config()
+require('dotenv').config();
 const path = require('path');
 
 const filePath = path.join(__dirname, 'Test.xlsx');
 const showConsoleMessages = true;
 
-xlsx2mongo.init(filePath);
+xlsx2mongo.init(filePath); // Initialize xlsx-mongo with the Excel file path
 
 const collectionName = 'test';
 
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(async () => {
-    
+    // Import data from the Excel file to the specified collection
     xlsx2mongo.import(collectionName, showConsoleMessages).then(() => {
-        mongoose.connection.close();
+        mongoose.connection.close(); // Close the MongoDB connection after import
     });
-    
+
+    // Add data from the Excel file to the specified collection
     xlsx2mongo.add(collectionName, filePath, showConsoleMessages).then(() => {
-        mongoose.connection.close();
+        mongoose.connection.close(); // Close the MongoDB connection after adding
     });
 
     // Avoid running both import and add at the same time
